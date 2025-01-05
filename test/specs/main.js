434ff18fd8001d6968585d736146c36893b38530
Talise Hendrix <trhendri1@gmail.com>
2025-01-05 01:54:45 -0600

    WIP

--- test/specs/main.js
+++ test/specs/main.js
@@ -16,40 +16,53 @@ const page = require("./page");
 });
 */
 
-
 //import { Key } from 'webdriverio'
 //Change to target instead of Amazon bc of constant captcha
-// WebDRIVER IO CRASH COURSE, TO DO:  move to its own project
-/*
+// WebDRIVER IO CRASH COURSE, TO DO:  move to its own project COMPLETE
+
 describe("Target", () => {
+/*
+    //runs before each It block
+    beforeEach(async () => { 
+        await browser.url("/"); 
+        await browser.execute(() => { 
+            localStorage.clear(); 
+            sessionStorage.clear(); 
+        }); 
+    }); 
+        
+       //Runs after each it block
+        afterEach(async () => { 
+        await browser.deleteCookies(); 
+        await browser.refresh(); 
+    });
+    */
+    
     it("Check browser URL and Title", async () => {
-        await browser.url("https://target.com");
+        await browser.url("/");
         await expect(browser).toHaveUrl("https://www.target.com/");
         //await expect(browser).toHaveTitleContaining('Amazon.com');
     });
 
-    
-it.only('Check Jest is working with no errors', async() =>{
-const four = 4;
-const five = 5;
-await console.log(four + five);
-await expect(four + five).toBeLessThan(20);
-
-}); 
+    /* 
+   it("Check Jest is working with no errors", async () => {
+        const four = 4;
+        const five = 5;
+        await console.log(four + five);
+        await expect(four + five).toBeLessThan(20);
+    });
+    */
 
     it("Search Content and Verify Text", async () => {
-        await browser.url("https://target.com");
-
-        const searchText = await $('div[data-test="resultsHeading"]');
-
-        const searchBox = await $('input[type="search"]');
-        const searchButton = await $('button[type="submit"]');
-        await searchBox.addValue("macbook");
-        await searchButton.click();
+        await browser.url("/");
+        await browser.refresh();
+        await page.keywordSearch();
+        const searchText = await $(page.searchText);
+        await browser.pause(2000);
         await searchText.waitForDisplayed();
         const confirmSearchText = await searchText.getText();
 
-        await console.log(confirmSearchText);
+        console.log(confirmSearchText);
 
         await expect(confirmSearchText).toContain("macbook");
     });
@@ -57,28 +70,32 @@ await expect(four + five).toBeLessThan(20);
     //Auto Suggestion Practice
 
     it("should check autosuggest", async () => {
-        await browser.url("https://www.target.com");
-        const searchBox = await $('input[type="search"]');
+        await browser.url("/");
+        const searchBox = await $(page.searchBox);
 
         await searchBox.click();
+        const autoSuggestBox = await $(page.autoSuggestBox);
+        await autoSuggestBox.waitForDisplayed();
 
         await browser.keys("ArrowDown");
+
         await browser.keys("ArrowDown");
-        await browser.pause(1000);
+        const matcher = await $$(".sc-bd5d6398-0.tsLvz")[1].getText();
+        await console.log(matcher);
 
         await browser.keys("Enter");
 
         //const afterSearchBox = await $('.gDYAEt"]');
         // await afterSearchBox.waitForDisplayed();
 
-        const searchText = await $('div[data-test="resultsHeading"]');
+        const searchText = await $(page.searchText);
         await searchText.waitForDisplayed();
         const confirmSearchText = await searchText.getText();
 
-        await expect(confirmSearchText).toContain("wrapping paper");
+        await expect(confirmSearchText).toContain(matcher);
         //Be sure to try alternative methods to grab text in variables.
     });
-}); */
+});
 
 //Verify the Add Cart Flow
 // Before Hooks
@@ -99,20 +116,24 @@ Flow:
 describe("Add to Cart Flow", () => {
     // Why should we add a before block here instead of just using it in it block? does it run before every it block if there were multiple?
     before(async () => {
-        await browser.url("https://target.com");
-        const searchBox = await $('input[type="search"]');
-        const searchButton = await $('button[type="submit"]');
-        await searchBox.addValue("macbook");
-        await searchButton.click();
+        await browser.url("/");
+        
     });
-    /*
+
     it("Add to Cart", async () => {
         //find container to work with multiple nodes
         // await $$('dafadsfa'); to select all nodes
         //await $$('dafadsfa')[0]; an array to select the first node
         //await $('dafadsfa'); just one dollar sign also selects the first item
-        const firstItem = await $$(".sc-e0eaa558-0")[0];
-        await firstItem.click();
+        await page.keywordSearch();
+        //Banner Keeps messing up test. Figure out workaround.
+        await browser.pause(8000);
+        const productListGrid = await $('div[data-module-type="ProductListGrid"]');
+        await productListGrid.click();
+       
+    
+        await page.addToCart();
+       
         const itemPrice = await $$('span[data-test="product-price"]')[0].getText();
         const addToCartButton = await $('button[aria-label="Add to cart for Unlimited Cellular HardShell Case for 13-inch MacBook Retina - Blue"]');
         await addToCartButton.click();
@@ -124,7 +145,7 @@ describe("Add to Cart Flow", () => {
         await viewCartButton.click();
         const subtotal = await $(".sc-93ec7147-3.hNHMW").getText();
         await expect(subtotal).toContain(itemPrice);
-    }); */
+    });
 
     //Update Cart and Verify Amount
     //Negative Assertions
@@ -137,6 +158,7 @@ Flow
 */
 
     it("Update Cart and Verify", async () => {
+       
         await page.addToCart();
         await browser.pause(2000);
         const closeButton = await $('button[aria-label="close"]');
--- test/specs/page.js
+++ test/specs/page.js
@@ -1,58 +1,39 @@
 module.exports = {
-
-four: 4,
-//Inputs
-flashAlert: ".success",
-usernameField: "#username",
-passwordField: "#password",
-
-//Buttons
-loginButton: 'button[type="submit"]',
-addElementButton: "button=Add Element",
-deleteButton: ".added-manually",
-
-//Functions
-
-loginApp: async function (username, password) {
-    await browser.url("/login");
-    const usernameField = await $(this.usernameField);
-    await usernameField.setValue("tomsmith");
-    const passwordField = await $(this.passwordField);
-    await passwordField.setValue("SuperSecretPassword!");
-    const loginButton = await $(this.loginButton);
-    await loginButton.click();
-},
-
-addElement: async function () {
-    await browser.url("/add_remove_elements/");
-    const addElementButton = await $(this.addElementButton);
-    await addElementButton.click();
-},
-
-deleteElement: async function () {
-    await $(this.deleteButton).click();
-},
-
-addToCart: async function () {
-    const firstItem = await $$(".sc-e0eaa558-0")[0];
-    await firstItem.click();
-    const addToCartButton = await $$('div.sc-529a2ea7-0 button[type="button"]')[1];
-    await addToCartButton.click();
-    const addCartModal = await $(".ReactModal__Content");
-    await addCartModal.waitForExist();
-},
-
-//logoutPage:
+    four: 4,
+    //Inputs
+    searchBox: 'input[type="search"]',
 
 
+    //Buttons
+    searchButton: 'button[type="submit"]',
 
+    searchText: 'div[data-test="resultsHeading"]',
 
+    //Modals
+    autoSuggestBox: '#typeahead',
 
+    firstItem:'.sc-e0eaa558-0',
 
+    //Functions
 
+    addToCart: async function () {
+        const firstItem = await $('.sc-e0eaa558-0');
+        
+        await firstItem.click();
+        await console.log("Afterclick");
+        const addToCartButton = await $$('div.sc-529a2ea7-0 button[type="button"]')[1];
+        await addToCartButton.click();
+        const addCartModal = await $(".ReactModal__Content");
+        await addCartModal.waitForExist();
+    },
 
+    //logoutPage:
+    //Search with keyword
 
-
-
-
+    keywordSearch: async function () {
+        const searchBox = await $(this.searchBox);
+        await searchBox.addValue("macbook");
+        const searchButton = await $(this.searchButton);
+        await searchButton.click();
+    },
 };
